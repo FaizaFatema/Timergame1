b7a62c7142b9f65f4521d72c36f990b02bb3fa5f
Faiza <faiza.fatema.1947@gmail.com>
2021-12-30 16:32:17 +0530

    UnityTime Code Project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TMP_Text textTimer;
+
+    private float timer = 10.0f;
+    public GameController gameController;
+
+    void Update()
+    {
+        if (timer <= 0)
+        {
+            timer = 0;
+            DisplayTime();
+            gameController.GameOver();
+            return;
+        }
+        timer -= Time.deltaTime;
+
+        DisplayTime();
+
+
+    }
+    void DisplayTime()
+    {
+        int seconds = Mathf.FloorToInt(timer);
+        textTimer.text = string.Format("{00}", seconds);
+    }
+}
